765b2a431ee7c48836b5b8985f3d2a2d7f931f31
emanuelediluzio <emanuelediluzio0@gmail.com>
2025-05-09 12:18:11 +0200

    Aggiunto next.config.js per ignorare test nelle dipendenze e script postinstall per rimuovere test da pdf-parse

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack: (config) => {
+    config.module.rules.push({
+      test: /test\/.*\.js$/,
+      use: 'null-loader'
+    });
+    return config;
+  }
+};
+
+module.exports = nextConfig; 
